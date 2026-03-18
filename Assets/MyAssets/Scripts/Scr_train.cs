d233929d7e32d44509694b00a5f061d59c0a74cc
Werdco <Werdco57632@gmail.com>
2026-03-17 23:48:48 -0400

    added grass, second offset train, and red flashing when trains pass

--- Assets/MyAssets/Scripts/Scr_train.cs
+++ Assets/MyAssets/Scripts/Scr_train.cs
@@ -10,6 +10,8 @@ public class Scr_train : MonoBehaviour
 
     public GameObject train; // the train object to use
     public TextMeshProUGUI timer; // the timer text to use
+    public Renderer hazard_indicator_renderer; // the renderer of the hazard indicator to show when train is passing
+
     public float speed = 100f; // how far the train moves per second
     public float time_wait = 10f; // how many seconds to wait between trains
 
@@ -17,13 +19,23 @@ public class Scr_train : MonoBehaviour
     public float track_length = 100f; // how long the track is
     public float buffer = 5f; // how much space to leave between the train and the edge of the track
 
+    public float hazard_pulse_length = 1f; // how many seconds the pulse of the hazard indicator should last
+    public float hazard_pulse_intensity = 0.5f; // the alpha value of the hazard indicator at the peak of a pulse (out of 1f)
+
+
 
     private float time_until_next = 0f; // how many frames until the train will arive
     private bool moving = false; // whether the train is currently moving or not
 
+    private float hazard_pulse_timer = 0f; // how many frames into a hazard pulse it is
+    private Color hazard_color = new Color(1f, 1f, 1f, 0f); // the current color of the hazard indicator
+
     void Start()
     {
         TrainArrive();
+
+        hazard_color = hazard_indicator_renderer.material.color;
+        hazard_color.a = 0f;
     }
 
 
@@ -33,7 +45,11 @@ public class Scr_train : MonoBehaviour
         if (moving) {
             train.transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
             CheckTrainLeft();
-        } else
+
+            if (hazard_pulse_timer <= 0) { hazard_pulse_timer = hazard_pulse_length; }
+        }
+
+        else
         {
             time_until_next -= Time.deltaTime;
 
@@ -43,6 +59,19 @@ public class Scr_train : MonoBehaviour
         }
         
         timer.text = "Next train: " + (Math.Ceiling(time_until_next)).ToString() + "s";
+        
+
+        hazard_pulse_timer = Math.Clamp(hazard_pulse_timer - Time.deltaTime, 0f, hazard_pulse_length);
+
+        if (hazard_pulse_length > 0)
+        {
+            hazard_color.a = Math.Clamp(MathMapPulse(hazard_pulse_timer / hazard_pulse_length) * hazard_pulse_intensity, 0f, 1f);
+        }
+        else
+        {
+            hazard_color.a = Math.Clamp(hazard_pulse_intensity, 0f, 1f);
+        }
+        hazard_indicator_renderer.material.color = hazard_color;
     }
 
 
@@ -69,4 +98,10 @@ public class Scr_train : MonoBehaviour
 
         return train_left;
     }
+
+    float MathMapPulse(float value) // maps a value from 0 to 1 to a pulse shape that goes from 0 to 1 and back to 0 in a smooth way
+    {         
+        return (float)(Math.Cos((value*2 + 1) * Math.PI) + 1) / 2;
+    }
+
 }
--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 ﻿# Train Simulator
 
- Latest Documentation Video: https://youtu.be/kW29mfmC0UA
+Latest Documentation Video:
+
+https://youtu.be/kW29mfmC0UA - Documentation #1
+(Documentation video is 1 version behind)
+
+
+
+
 
 
 
 Built on UnityXRT - Intended for use with a VR headset
+
+Current features:
+
+
 
 Current features:
 
@@ -17,4 +28,10 @@ Current features:
 
 * use TrainArive() to trigger a train passing (currently called when the train timer hits 0)
 * use CheckTrainLeft() to identify when a train has finished passing (currently also resets the timer)
+
+
+
+* grassy field and shrubbery
+* two trains with offset timings
+* flashing indicator when train passing
 
